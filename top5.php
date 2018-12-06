9bb44a7cbc7e804e901b4bb6ce4fdfe4cdc2586f
admin <admin@USCS-Mac184.local>
2018-12-06 15:51:51 -0800

    top 5 products from member websites

---
+++ top5.php
@@ -0,0 +1,59 @@
+<?php
+
+//Program to display top5 products in the member websites
+// and json encode the result
+include_once 'includes/config.php';
+
+
+$query = "SELECT p.id, p.prod_name, p.prod_description, SUM(star) as top "
+        . "FROM products p, `product_rating` pr "
+        . "WHERE p.id = pr.product_id "
+        . "GROUP BY product_id "
+        . "ORDER BY top DESC LIMIT 5";
+
+$result = mysqli_query($link, $query);
+
+$top5 = array();
+$item = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+     $item["product_id"] = $row['id'];
+     $item["product_name"] = $row['prod_name'];
+     $item["product_description"] = utf8_encode(truncate_text($row['prod_description']));
+     $item["rating"] = $row['top'];
+     $top5[] = $item;
+}
+
+$json = json_encode($top5);
+
+
+
+print $json;
+function truncate_text($str, $limit=100) {
+    if (strlen($str) <= $limit) return $str;
+	
+	if (false !== ($max = strpos($str, ".", $limit))) {
+		 
+		if ($max < strlen($str) - 1) {
+			
+			$str = substr($str, 0, $max) . "...";
+			
+		}
+		
+	}
+
+        return $str;
+}
+
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
+}
+
+?>
---
+++ top5_read.php
@@ -0,0 +1,35 @@
+<?php
+//program for market place
+//Read the json encoded top 5 products from member websites
+//json_decode and process
+$url= "http://localhost/top5.php";
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+
+
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $data = curl_exec($ch);
+    
+
+
+curl_close($ch);
+$products = json_decode($data);
+
+
+
+foreach ($products as $product) {
+    print $product->product_id;
+    print " , ";
+    print $product->product_name;
+    print " , ";
+    print $product->product_description;
+    print " , ";
+    print $product->rating;
+    print "<br>";
+}
+
+?>
