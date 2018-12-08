bbd87ce0d02df034c751877ebeac5661d4f2e4c4
admin <admin@USCS-Mac184.local>
2018-12-07 20:04:46 -0800

    mail

--- cart_checkout.php
+++ cart_checkout.php
@@ -9,21 +9,22 @@ if (isset($_POST['cart_id'])) {
     $cart_id = $_POST['cart_id'];
 }  
 
+$cart_id = 3;
    
-$query = "SELECT p.product_id, p.product_name, p.product_price, s.product_quantity "
-        . "FROM products p, `shopping_cart` s "
-        . "WHERE  p.product_id = s.product_id AND s.cart_id=".$cart_id;
+$query = "SELECT p.id, p.prod_name, p.prod_price, s.quantity FROM products p, `shopping_cart` s WHERE p.id = s.product_id AND s.cart_id=".$cart_id;
+//print $query;
   $result = mysqli_query($link, $query);
   
  
 
 
 if (!empty($result)) {
+    
     while ($row = mysqli_fetch_assoc($result)) {
-         $item["product_id"] = $row['product_id'];
-         $item["product_name"] = $row['product_name'];
-         $item["product_price"] = $row['product_price'];
-         $item["product_quantity"] = $row['product_quantity'];
+         $item["product_id"] = $row['id'];
+         $item["product_name"] = $row['prod_name'];
+         $item["product_price"] = $row['prod_price'];
+         $item["product_quantity"] = $row['quantity'];
          $top5[] = $item;
     }
     
@@ -37,14 +38,16 @@ if (!empty($result)) {
             . '</tr>';
     foreach($top5 as $item) {
         $messagestr .= '<tr>';
-        $messagestr .=  '<td>'.$item->product_name.'</td>';
-        $messagestr .=  '<td>'.$item->product_price.'</td>';
-        $messagestr .=  '<td>'.$item->quantity.'</td>';
-        $messagestr .=  '<td>'.$item->product_price * $item->product_quantity.'</td>';
+        $messagestr .=  '<td>'.$item['product_name'].'</td>';
+        $messagestr .=  '<td>'.$item['product_price'].'</td>';
+        $messagestr .=  '<td>'.$item['quantity'].'</td>';
+        $messagestr .=  '<td>'.$item['product_price'] * $item['product_quantity'].'</td>';
        
        $messagestr .=   '</tr>';
     }
     $messagestr .= '</table>';
+}else {
+    $messagestr = "No products found";
 }
   
  
@@ -53,7 +56,7 @@ if (!empty($result)) {
     
 if(isset($_POST['submit']))
 {
-    $cart_string = $_POST['cart_string'];
+    
     
     $to = 'supriyajain3010@gmail.com';
     $firstname = $_POST["name"];
@@ -65,7 +68,7 @@ if(isset($_POST['submit']))
     $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $message ='<table style="width:100%">
+    $message .='<table style="width:100%">
         <tr>
             <td>'.$firstname.'</td>
         </tr>
@@ -83,11 +86,14 @@ if(isset($_POST['submit']))
     
     $message = $messagestr;
     $message .= '</table>';
+    
+    
+    print $message;
 
     if (mail($to, $email, $message, $headers))
     {
         $msg=  'Thank you. Your Order has been sent';
-        header("Location: products.php?$msg");
+       # header("Location: products.php?$msg");
         
     }else{
         echo 'failed';
